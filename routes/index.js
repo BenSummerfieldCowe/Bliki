8188c637f3b9d72dae3cb3fc39f97fe4389b5c23
N440708 <ben.summerfield-crowe@ba.com>
2017-06-05 16:32:29 +0100

    Adding very minimal display template for posts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var renderData={};
+  var posts={};
+  //Hard coding dummy posts until db setup
+  posts=[{content:'This is the first post \n see', title:'Post 1'},{content:'this is the second post ok',title:'the difficult second post'}]
+  renderData.posts=posts;
+  renderData.title='Express';
+  res.render('index', renderData);
 });
 
 module.exports = router;
