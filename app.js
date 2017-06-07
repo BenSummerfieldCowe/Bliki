1311c4883c0c44aab323c7f2ff6896fb813da9cd
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-07 17:54:49 +0100

    Fixing partial location for new name

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-hbs.registerPartials(path.join(__dirname,'views', 'partials'));
+hbs.registerPartials(path.join(__dirname,'views', 'partial'));
 hbs.registerHelper('shortenForHome',handlebarHelpers.shortenForHome);
 hbs.registerHelper('createURLForPost',handlebarHelpers.createURLForPost);
 
