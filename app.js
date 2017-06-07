3c2831ca577a33946caf8bef08a67c083271715d
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-07 14:48:47 +0100

    Updated routing for an individual post

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var post = require('./routes/posts');
+var handlebarHelpers = require(path.join(__dirname,'views', 'helpers'));
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname,'views', 'partials'));
+hbs.registerHelper('shortenForHome',handlebarHelpers.shortenForHome);
+hbs.registerHelper('createURLForPost',handlebarHelpers.createURLForPost);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/post', post);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
