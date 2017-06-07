b585f9acc22e01208cb1acfb6efafb14b22c210a
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-07 18:22:47 +0100

    Cleaning up some small lines and adding routing and page for an indidual post

--- routes/posts.js
+++ routes/posts.js
@@ -2,17 +2,11 @@ var express = require('express');
 var router = express.Router();
 var getPostByTitle = require('../lib/posts').getPostByTitle;
 
-/* GET home page. */
+/* GET individual blog page. */
 router.get('/:title/', function(req, res, next) {
   var renderData={};
-  renderData.post=getPostByTitle(req.params.title);
+  renderData.post=getPostByTitle(req.params.title)[0];
   res.render('post', renderData);
-});
-/* GET home page. */
-router.get('/:year([0-9]{4})/:month([0-9]{2})/:day([0-9]{2])/:title/', function(req, res, next) {
-  var renderData={};
-  console.log(req.params.title);
-  res.render('index', renderData);
 });
 
 module.exports = router;
