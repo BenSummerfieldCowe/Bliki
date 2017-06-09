15988b2257f2f60272e49b3f6c87a566dc3435b2
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-09 20:46:04 +0100

    getPosts now returns max page and returns an error if navigating to incorrect page number

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,19 @@ router.get('/:pageNo([0-9]+)?', function(req, res, next) {
   var pageNo = req.params.pageNo ? req.params.pageNo : 1;
   var renderData={};
   var posts={};
-  posts=getPosts(pageNo);
-  renderData.posts=posts;
+  var postsForPage = getPosts(pageNo);
+  renderData.maxPage = postsForPage.maxPage
+  console.log(postsForPage.maxPage)
+  renderData.posts=postsForPage.posts;
+  renderData.pageNo = pageNo;
   renderData.title='Express';
-  res.render('index', renderData);
+  if (postsForPage.err){
+    console.log('error')
+    next(postsForPage.err);
+  } else {
+    console.log('index')
+    res.render('index', renderData);
+  }
 });
 
 module.exports = router;
