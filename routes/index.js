6fc32b824965c7294589ca9c466c37b9570496e4
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-09 16:06:50 +0100

    Adding for page numbers on home page

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var getPosts = require('../lib/posts').getPosts();
+var getPosts = require('../lib/posts').getPosts;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:pageNo([0-9]+)?', function(req, res, next) {
+  var pageNo = req.params.pageNo ? req.params.pageNo : 1;
   var renderData={};
   var posts={};
-  posts=getPosts;  
+  posts=getPosts(pageNo);
   renderData.posts=posts;
   renderData.title='Express';
   res.render('index', renderData);
