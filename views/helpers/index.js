211e14514ca66468c6a2fbd25d6180bb120e2a7f
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-10 12:16:24 +0100

    Improving pagination and adding more test posts

--- views/helpers/index.js
+++ views/helpers/index.js
@@ -1,8 +1,8 @@
 
 function shortenForHome(content) {
   //def clean this up
-  if(content.length > 500) {
-    return content.substring(0,content.substring(500).indexOf(" ")+500) +' ...';
+  if (content.length > 500) {
+    return content.substring(0, content.substring(500).indexOf(" ") + 500) + ' ...';
   }
   return content
 }
@@ -14,17 +14,34 @@ function createURLForPost(title) {
   return title.replace(/ /g, '_').toLowerCase();
 }
 
-function pagination(current,max) {
-  if(max===1) {
+function pagination(current, max) {
+  if (max === 1) {
     return
   }
-  var pagination = "<a href='/'>1</a>"
-  var i;
-  //TODO clean up better format, limit amount of pages, show surrunding numbers, i.e 1 2 3 4 or 1 .. 4 5 6 .. 10 etc
-  for (i=2; i<max; i++) {
-     pagination += " <a href='"+i+"'>"+i+"</a>"
-   }
-   pagination += " <a href='"+max+"'>"+max+"</a>"
+  current = parseInt(current);
+  var makeLink = function (page) {
+    if (current === page) {
+      return "<a class='current' href='" + page + "'>" + page + "</a> "
+    } else return "<a href='" + page + "'>" + page + "</a> "
+  }
+  var pagination = makeLink(1);
+  var j = current - 2;
+  if (j > 2) {
+    pagination += ".."
+  }
+  var i = 2 + current
+  for (j; j <= i; j++) {
+    if (j <= 1) {
+      continue;
+    } else if (j >= max) {
+      break;
+    }
+    pagination += makeLink(j);
+  }
+  if (j < max - 1) {
+    pagination += "..";
+  }
+  pagination += makeLink(max);
   return pagination;
 }
 
