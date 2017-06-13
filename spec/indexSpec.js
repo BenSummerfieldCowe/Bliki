e4be87f96230d7582ed46a63286c22647a51644a
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-13 17:57:23 +0100

    Trying zombie

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,14 +1,34 @@
+'use strict'
 var posts = require('../lib/posts.js');
 var request = require("request");
+var Browser = require('zombie');
 
-  var base_url = "http://localhost:3000"
+const browser = new Browser();
+// Browser.localhost('localhost', 3000);
+
+//TODO add mocking/spys as it wont always use the local
+ 
+describe("Index Page", function(){
+  it("contains #content div", function(done){
+    browser.visit("http://localhost:3000/", function(){
+      assert.ok(browser.success);
+      done();
+    });
+  });
+});   
 
 describe("The Home page", function () { 
-  //This needs expanding a bunch
-    it("returns can be successfully requested ", function(done) {
-        request.get(base_url, function(err,res,body) {
-            expect(res.statusCode).toBe(200);
-            done();
-        })
+    const browser = new Browser();
+    it("can be successfully requested ", function(done) {
+        browser.visit('/',function(err) {
+        expect(browser.assert.success());
     });
+    expect(browser.assert.success());
+        done();
+    });
+
+    // it("displays posts", function(done) {
+    //     browser.assert.text('.title','The first post');
+    //     done();
+    // });
 });
