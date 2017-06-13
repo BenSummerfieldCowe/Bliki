cd87eadc226de1e3f66064efce0c62b3808d172f
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-13 23:02:47 +0100

    Fixed test issue and written some starting tests

--- spec/indexSpec.js
+++ spec/indexSpec.js
@@ -1,49 +1,32 @@
 'use strict'
 //var posts = require('../lib/posts.js');
-var request = require("request");
 var Browser = require("zombie");
-
-const browser = new Browser();
-// Browser.localhost('localhost', 3000);
 var base_url = 'http://localhost:3000';
 
 //TODO add mocking/spys as it wont always use the local
- 
-// describe("Index Page", function(){
-//   it("contains #content div", function(done){
-//     browser.visit("http://localhost:3000/", function(){
-//       assert.ok(browser.success);
-//       done();
-//     });
-//   });
-// });   
-
-describe("The Home page", function () { 
+ describe("The blog page", function () { 
     const browser = new Browser();
-    beforeEach(function(done) {
-      browser.visit('http://localhost:3000/',done);
-    });
 
-    it("can be successfully requested ", function(done) {
-      console.log('y')
-        // browser.visit('/',function(err) {
-         console.log('yo');
-          console.log(browser.text());
-    //     expect(browser.assert.success());    
-    //     expect(browser.assert.success());
-    //  });
+    it("can be successfully requested", function(done) {
+      browser.visit('http://localhost:3000/',function() {
+      expect(browser.success).toBe(true)
+      expect(browser.status).toBe(200)
       done();
-    });
-  //This needs expanding a bunch
-    it("returns can be successfully requested ", function(done) {
-        request.get(base_url, function(err,res) {
-            expect(res.statusCode).toBe(200);
-            done();
-        })
+      });
     });
 
-    // it("displays posts", function(done) {
-    //     browser.assert.text('.title','The first post');
-    //     done();
-    // });
+    it("displays posts", function(done) {
+      browser.visit('http://localhost:3000/',function() {
+      expect(browser.querySelector(".title").textContent).toBe('The first post');
+      done();
+      });
+    })
+
+    it("can be succesfully requested for later posts", function(done) {
+      browser.visit('http://localhost:3000/2',function() {
+      expect(browser.success).toBe(true)
+      expect(browser.status).toBe(200)
+      done();
+      })
+    });
 });
