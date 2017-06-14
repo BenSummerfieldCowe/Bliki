812256858c0eb4348354bc15c6ef78906b995068
BenSummerfieldCowe <redtrakor@googlemail.com>
2017-06-14 19:26:41 +0100

    Removing unused code

--- spec/blogAndRouting.spec.js
+++ spec/blogAndRouting.spec.js
@@ -78,12 +78,3 @@ describe("The individual post page", () => {
   });
 
 });
-
-// var textAssertionOnPage = (browser, selector, assertion) => {
-//   var element = browser.querySelector(selector);
-//   if(element) {
-//     expect(element.textContent).toBe(assertion);
-//   } else {
-//     expect(false).toEqual(true, `element with selector ${selector} not found on page`);
-//   }
-// };
